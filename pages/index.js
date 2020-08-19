0ef37e2a96bf1d407037e41d737c376d8d129a2e
estellar_boyz <tayalone@gmail.com>
2020-08-19 23:01:19 +0700

    :sparkles: ใส่ action ให้ กับ input ต่างๆ

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,54 @@ import React, { useState } from 'react'
 
 function index() {
   const [list, setList] = useState('')
+  const [seachTarget, setSearchTarget] = useState('')
+  const [searchType, setSearchType] = useState('LINEAR_SEARCH')
+  const [result, setResult] = useState([])
 
-  const hangerOnChangedList = (e) => {
+  const handlerOnChangedList = (e) => {
     const newList = e.target.value
     setList(newList)
+  }
+
+  const handlerOnChangedSearchTarget = (e) => {
+    const newSeachTarget = e.target.value
+    setSearchTarget(newSeachTarget)
+  }
+
+  const handlerOnChangeSearchType = (e) => {
+    const newSearchType = e.target.value
+    setSearchType(newSearchType)
+  }
+
+  const handlerOnClickSubmitBtn = () => {
+    alert('click submit')
   }
 
   return (
     <div>
       Welcome to Next.js!!!
       <div>
+        <h1>Debug</h1>
+        <div>List: {list}</div>
+        <div>Search Target: {seachTarget}</div>
+        <div>Search Type: {searchType}</div>
+        <div>Result: {JSON.stringify(result)}</div>
+      </div>
+      <div>
         <label>List</label>
-        <input onChange={hangerOnChangedList} />
+        <input onChange={handlerOnChangedList} />
       </div>
       <div>
         <label>ค้นหา</label>
-        <select>
-          <option value="grapefruit">Grapefruit</option>
-          <option value="lime">Lime</option>
-          <option value="coconut">Coconut</option>
-          <option value="mango">Mango</option>
+        <input onChange={handlerOnChangedSearchTarget} />
+        <button onClick={handlerOnClickSubmitBtn}> ค้นหา </button>
+      </div>
+      <div>
+        <label>ประเภทการค้นหา</label>
+        <select value={searchType} onChange={handlerOnChangeSearchType}>
+          <option value="LINEAR_SEARCH">1. Linear Search</option>
+          <option value="BINARY_SEARCH">2. Binary Search</option>
+          <option value="BUBBULE_SEARCH">3. Bubble Search</option>
         </select>
       </div>
       <div>
