0c9f4a3e1fe299d8296b961753b3b1fdd00db207
estellar_boyz <tayalone@gmail.com>
2020-08-19 22:33:58 +0700

    sparkles: สร้างโครงสร้างพื้นฐานจบ
    git commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// const regex = /(\d+)(,\s*\d+)*/gm
+// const regexObj = new RegExp(regex)
+
+// var patt = new RegExp(/(\d+)(,\s*\d+)*/)
 
 function index() {
-  return <div>Welcome to Next.js!!!</div>
+  const [list, setList] = useState('')
+
+  const hangerOnChangedList = (e) => {
+    const newList = e.target.value
+    setList(newList)
+  }
+
+  const splitList = list.split(',')
+
+  return (
+    <div>
+      Welcome to Next.js!!!
+      <div>
+        <label>List</label>
+        <input onChange={hangerOnChangedList} />
+      </div>
+      <div>
+        <label>ค้นหา</label>
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      </div>
+      <div>
+        <label>Result</label>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default index
